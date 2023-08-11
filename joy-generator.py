b867cd19d6cf289f5fd69abc97184401d903a9af
Jake Siney <jakesiney@gmail.com>
2023-08-11 18:22:39 +0100

    All done

--- joy-generator.py
+++ joy-generator.py
@@ -3,10 +3,10 @@ import random
 # The Joy Generator
 
 # Complete list of people in the July 23 onsite cohort.
-cohort = ["Roberto", "David", "Denise", "Ellie", "Muhammad", "Andrew", "Ami", "Alina", "Emily", "Alex", "Ben", "Senthooran", "Kumani", "Huda", "Carolina", "Zubayda", "Rikie", "Mohsina", "Sarah", "Lateef", "Yasien", "Jake"]
+cohort = ["Roberto", "David", "Denise", "Ellie", "Muhammad", "Andrew", "Ami", "Alina", "Emily", "Alex", "Ben", "Senthooran", "Kumani", "Huda", "Carolina", "Zubayda", "Rikie", "Mohsina", "Sarah", "Lateef", "Yasien", "Jake", "Mohsin"]
 
 # A list of the people who have already done the daily joy challenge.
-joy_bringers = ["Ben", "Rikie", "Ellie", "Kumani"]
+joy_bringers = ["Ben", "Rikie", "Ellie", "Kumani", "Senthooran", "Emily", "Ami", "Denise", "Mohsina", "Sarah", "Jake", "Huda", "Zubayda", "Alex", "Carolina", "Andrew", "Alina", "Roberto", "Denise"]
 
 # A list of people who have not done the daily joy challenge yet.
 to_bring_the_joy = [person for person in cohort if person not in joy_bringers]
